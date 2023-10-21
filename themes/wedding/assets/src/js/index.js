248c15f645622ec815084f106ab6140c4ecb9eea
Andrés Patiño <andres@frontporchsolutions.com>
2023-10-21 13:33:59 -0500

    feat: :sparkles: Restore maps

--- themes/wedding/assets/src/js/index.js
+++ themes/wedding/assets/src/js/index.js
@@ -1,7 +1,5 @@
 import '../css/main.css';
 import '../js/base.js';
 import '../js/countdown.js';
-import '../js/modal.js'
-//import '../js/map.js';
-
-
+import '../js/modal.js';
+import '../js/map.js';
