fd7b9b64f5011fddb4d682645ab6f22d8491321b
Andrés Patiño <andres@frontporchsolutions.com>
2024-01-19 16:05:11 -0500

    feat: :sparkles: Invitados - sortable columns

--- themes/wedding/inc/functions/custom-columns.php
+++ themes/wedding/inc/functions/custom-columns.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 add_filter('manage_invitado_posts_columns', 'reorganizar_agregar_columna_invitado');
 function reorganizar_agregar_columna_invitado($columns) {
@@ -41,3 +41,36 @@ function mostrar_valores_columnas_personalizadas_estado($column, $post_id) {
         }
     }
 }
+
+
+add_filter('manage_edit-invitado_sortable_columns', 'sortable_columns_invitado');
+function sortable_columns_invitado($columns) {
+    $columns['asistencia'] = 'asistencia';
+    $columns['acompanado'] = 'acompanado';
+    $columns['restriccion'] = 'restriccion';
+    $columns['indicaciones'] = 'indicaciones';
+
+    return $columns;
+}
+
+add_action('pre_get_posts', 'orderby_custom_columns_invitado');
+function orderby_custom_columns_invitado($query) {
+    if(!is_admin() || !$query->is_main_query()) {
+        return;
+    }
+
+    if ($query->get('orderby') == 'asistencia') {
+        $query->set('meta_key', 'asistencia');
+        $query->set('orderby', 'meta_value');
+    } elseif ($query->get('orderby') == 'acompanado') {
+        $query->set('meta_key', 'acompanado');
+        $query->set('orderby', 'meta_value');
+    } elseif ($query->get('orderby') == 'restriccion') {
+        $query->set('meta_key', 'restriccion');
+        $query->set('orderby', 'meta_value');
+    } elseif ($query->get('orderby') == 'indicaciones') {
+        $query->set('meta_key', 'indicaciones');
+        $query->set('orderby', 'meta_value');
+    }
+}
+
