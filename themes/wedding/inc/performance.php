26ab27754127490edbedc13f9d7c246998c6ca9a
Andrés Patiño <andres@frontporchsolutions.com>
2024-01-20 13:13:24 -0500

    feat: :zap: lazy loading

--- themes/wedding/inc/performance.php
+++ themes/wedding/inc/performance.php
@@ -16,11 +16,3 @@ function themeWedding_prefix_output_callback($buffer)
 {
     return preg_replace("%[ ]type=[\'\"]text\/(javascript|css)[\'\"]%", '', $buffer);
 }
-
-
-// Add `loading="lazy"` attribute to images output by the_post_thumbnail().
-function themeWedding_modify_post_thumbnail_html($html, $post_id, $post_thumbnail_id, $size, $attr)
-{
-    return str_replace('<img', '<img loading="lazy"', $html);
-}
-add_filter('post_thumbnail_html', 'themeWedding_modify_post_thumbnail_html', 10, 5);
